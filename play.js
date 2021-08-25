a827c8dba13306d3ffba2b68167c73687c425a7f
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-25 20:52:52 +0000

    Create client.js and update play.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require("net");
+// const net = require("net");
+const {connect}  = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', 
-    port: 50542  // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log('here I am');
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
