b5071e95f69abeec193fdfd360b278be06c7f5eb
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-26 00:11:25 +0000

    add possibility to use keybord to operate the snake

--- input.js
+++ input.js
@@ -1,10 +1,24 @@
+let connection;
+
+
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  }else if(data === 'w'){
+    connection.write('Move: up');
+  }else if(data === 'a'){
+    connection.write('Move: left');
+  }else if(data === 's'){
+    connection.write('Move: down');
+  }else if(data === 'd'){
+    connection.write('Move: right');
   }
+
 };
 
-const setupInput = function () {
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +27,16 @@ const setupInput = function () {
   
   return stdin;
 };
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+  
+//   return stdin;
+// };
 
 module.exports = {
   setupInput
